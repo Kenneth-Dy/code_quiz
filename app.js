49f39ed55eb25099d1f8caee16c04e245404e6e7
Kenneth Dy <kennethndy@yahoo.com>
2021-02-17 20:45:40 -0800

    added final score and highscore page

--- app.js
+++ app.js
@@ -1,40 +1,36 @@
 let i = 0
 const questions_answers = [
   {
-    "question": "Question 1",
-    "answer_1": "Answer 1",
-    "answer_2": "Answer 2",
-    "answer_3": "Answer 3",
-    "answer_4": "Answer 4",
-    "answer": "answer 1"
-
+    question: "What is a string?",
+    answer_1: "letters dwqdww wwwww  wwwwwwwwwww wwwwwwwwwwwwwwwww wwwwwwwwwwww wwwwww wwwwwwwww wwwww",
+    answer_2: "Answer 2",
+    answer_3: "Answer 3",
+    answer_4: "Answer 4",
+    answer: "answer 1"
   },
   {
-    "question": "Question 2",
-    "answer_1": "Answer 1",
-    "answer_2": "Answer 2",
-    "answer_3": "Answer 3",
-    "answer_4": "Answer 4",
-    "answer": "answer 2"
-
+    question: "Question 2",
+    answer_1: "Answer 1",
+    answer_2: "Answer 2",
+    answer_3: "Answer 3",
+    answer_4: "Answer 4",
+    answer: "answer 2"
   },
   {
-    "question": "Question 3",
-    "answer_1": "Answer 1",
-    "answer_2": "Answer 2",
-    "answer_3": "Answer 3",
-    "answer_4": "Answer 4",
-    "answer": "answer 3"
-
+    question: "Question 3",
+    answer_1: "Answer 1",
+    answer_2: "Answer 2",
+    answer_3: "Answer 3",
+    answer_4: "Answer 4",
+    answer: "answer 2"
   },
   {
-    "question": "Question 4",
-    "answer_1": "Answer 1",
-    "answer_2": "Answer 2",
-    "answer_3": "Answer 3",
-    "answer_4": "Answer 4",
-    "answer": "answer 4"
-
+    question: "Question 4",
+    answer_1: "Answer 1",
+    answer_2: "Answer 2",
+    answer_3: "Answer 3",
+    answer_4: "Answer 4",
+    answer: "answer 2"
   },
 ]
 
@@ -65,7 +61,7 @@ const questions_answers = [
 //   })
 // }
 
-let quiz_func = () => {
+let quiz_render = () => {
   document.getElementById('quiz_page').innerHTML = `
     <div class="row justify-content-center">
       <div class="col-sm-6">
@@ -75,18 +71,91 @@ let quiz_func = () => {
     <div class="row justify-content-center">
       <div class="col-sm-6">
         <div class="btn-group-vertical" role="group" aria-label="answer button group">
-          <button type="button" class="btn btn-outline-primary btn-lg ans_btn" id="answer_1">${questions_answers[i].answer_1}</button>
-          <button type="button" class="btn btn-outline-primary btn-lg ans_btn" id="answer_2">${questions_answers[i].answer_2}</button>
-          <button type="button" class="btn btn-outline-primary btn-lg ans_btn" id="answer_3">${questions_answers[i].answer_3}</button>
-          <button type="button" class="btn btn-outline-primary btn-lg ans_btn" id="answer_4">${questions_answers[i].answer_4}</button>
+          <button type="button" class="btn btn-outline-primary btn-lg ans_btn mb-1" id="answer_1">${questions_answers[i].answer_1}</button>
+          <button type="button" class="btn btn-outline-primary btn-lg ans_btn mb-1" id="answer_2">${questions_answers[i].answer_2}</button>
+          <button type="button" class="btn btn-outline-primary btn-lg ans_btn mb-1" id="answer_3">${questions_answers[i].answer_3}</button>
+          <button type="button" class="btn btn-outline-primary btn-lg ans_btn mb-1" id="answer_4">${questions_answers[i].answer_4}</button>
         </div>
       </div>  
     </div>`
 }
 
+let final_score_render = () => {
+  document.getElementById('quiz_page').innerHTML =`
+    <div class="row justify-content-center">
+      <div class="row justify-content-center">
+        <div class="col-sm-6">
+          <h1 class="display-4 text-center fw-bold">Your Final Score is: <h1>
+          <h1 class="display-4 text-center fw-bold" id="final_score"></h1>
+          <form>
+            <div class="mb-3">
+              <label for="initials" class="form-label">Enter Initials</label>
+              <input type="text" name="initials" class="form-control" id="initials">
+            </div>
+            <button type="submit" class="btn btn-primary submit_btn" id="submit_btn">Submit</button>
+          </form>
+        </div>
+      </div>      
+    </div>
+  `
+  document.getElementById('navbar').innerHTML = ``
+  
+  document.getElementById('submit_btn').addEventListener('click', event => {
+    event.preventDefault()
+    highscore_render()
+    // document.getElementById('quiz_page').innerHTML = `
+    //       <h1>Is it working?</h1>
+    //       <h1>${document.getElementById('initials').value}</h1>`
+  })
+}
+
+let highscore_render = () => {
+  document.getElementById('quiz_page').innerHTML =`
+    <div class="row justify-content-center">
+      <div class="col-sm-6">
+        <h1 class="display-1 text-center fw-bolder">Highscores</h1>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-sm-6">
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Initial</th>
+              <th scope="col">Score</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td colspan="2">Larry the Bird</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-sm-6">
+        <button type="button" class="btn btn-primary">Start Over</button>
+        <button type="button" class="btn btn-primary">Reset Highscores</button>
+      </div>
+    </div>`
+}
+
 document.getElementById('start_btn').addEventListener('click', event => {
   i = 0
-  quiz_func()
+  quiz_render()
   i++
   // document.getElementById('quiz_page').innerHTML = `
   //     <div class="row justify-content-center">
@@ -111,11 +180,18 @@ document.getElementById('start_btn').addEventListener('click', event => {
 document.addEventListener('click', event => {
   if (event.target.classList.contains('ans_btn')) {
     if(i >= questions_answers.length){
-      document.getElementById('quiz_page').innerHTML = `<h1>You Did It!</h1>`
+      final_score_render()
+      // document.getElementById('submit_btn').addEventListener('click', event => {
+      //   event.preventDefault()
+
+      //   document.getElementById('quiz_page').innerHTML = `
+      //     <h1>Is it working?</h1>
+      //     <h1>${document.getElementById('initials').value}</h1>`
+      // })
     }else{
-      quiz_func()
+      quiz_render()
       i++
-    }
-    
+    }    
   }
 })
+
